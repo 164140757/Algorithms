db4341a30b18f4aff110af46c4e155a33401b599
WhiteDream <43661768+164140757@users.noreply.github.com>
2019-04-21 19:37:02 +0800

    df

---
+++ src/main/java/MyjavaTest/App.java
@@ -0,0 +1,13 @@
+package MyjavaTest;
+import edu.princeton.cs.algs4.*;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
