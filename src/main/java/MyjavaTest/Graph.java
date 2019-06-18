939a958837ed479aa5c273f35678e06aef073067
164140757 <164140757@qq.com>
2019-06-18 16:59:12 +0800

    Graph

---
+++ src/main/java/MyjavaTest/Graph.java
@@ -0,0 +1,160 @@
+package MyjavaTest;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class Graph{//double ->WeightType of Edges
+  private int V;//cannot be modified
+  private int E;//number of edges
+  private List<Node> headNodeList;
+  //inner class & Generic Class
+  private class Node{
+     private double weight;//for edges
+     private String tag;
+     private Node next;
+     private int index;
+
+     public Node(){
+        index = -1;
+        next = null;
+        tag = "#";
+        //weight ->null
+     }
+     public int getIndex() {
+        return index;
+     }
+     public void setIndex(int index){
+       this.index = index;
+     }
+     public void setNext(Node next){
+       this.next = next;
+     }
+     public Node getNext(){
+       return next;
+     }
+     public String getTag(){
+       return tag;
+     }
+     
+    public void setTag(String tag) {
+      this.tag = tag;
+    }
+    public double getWeight(){
+      return weight;
+    }
+    public void setWeight(double weight){
+       this.weight = weight;
+     } 
+  }
+
+  //constructor of empty adjacency list
+  public Graph(int vertex) {
+    E = 0;
+    if(V<0) throw new IllegalArgumentException("Number of vertices must be nonnegative");
+    this.V = vertex;
+    headNodeList = new ArrayList<>(V);//headlist with space & ArrayList to instantiate List
+    //List<Node> headNodeList - > cannot visit class Graph's field instead -> a new list 
+    for (int i=0;i<V;i++){//nodes -> list
+      Node node = new Node();
+      node.setIndex(i);
+      node.setNext(null);//only null in java
+      headNodeList.add(node);
+    }
+  }
+  
+  public Graph(In in){
+    try{
+      this.V = in.readInt();
+      if(V<0) throw new IllegalArgumentException("Number of vertices must be nonnegative");
+      this.E = in.readInt();
+      if(E<0) throw new IllegalArgumentException("Number of edges must be nonnegative");
+      headNodeList = new ArrayList<>(V);//()gives the initalCapacity
+      for (int i=0;i<V;i++){//nodes -> list
+        Node node = new Node();
+        node.setIndex(i);
+        node.setNext(null);//only null in java
+        headNodeList.add(node);
+      }
+      //add tags
+      String init = "A";
+      for(int i=0;i<V;i++){
+        headNodeList.get(i).setTag(init);
+        char temp = init.charAt(0);
+        int ascii = (int) temp +1;
+        char tChar = (char)ascii;
+        init = Character.toString(tChar);
+      }
+      //add Edges
+      for (int i =0;i<E;i++){
+        int v = in.readInt();
+        int u = in.readInt();
+        double w = in.readDouble();
+        validateVertex(v);
+        validateVertex(u);
+        addEdge(v, u, w);
+      }
+    }
+    catch(NoSuchElementException e){
+      throw new IllegalArgumentException("invalid input format in Graph constructor");
+    }
+  }
+
+  //methods
+  //{@code 0<= v <= V}
+  private void validateVertex(int v){
+    if(v<0 || v>V) throw new IllegalArgumentException("vertex"+V+"is not between 0 and"+(V-1));
+    //() make sure parameter given to IllegalArgumentException is a string
+  }
+  public void addEdge(int v,int u,double VALUE){
+    validateVertex(v);
+    validateVertex(u);
+    Node node = new Node();
+    node = headNodeList.get(v);//headNode[v] & get(i) returns the element at the position i
+    while(node.getNext()!=null){//headNode(n-1) already exits
+      node = node.next;
+      int index = node.getIndex();
+      if(index == u ) {node.setWeight(VALUE);return;}
+    }
+    //headNode(n) & for new node to insert in the end of list
+    if(node.getIndex()==u){node.setWeight(VALUE);return;}
+    //headNode[u][v] does not exit
+    Node Nnode = new Node();
+    Nnode.setIndex(u);
+    Nnode.setWeight(VALUE);
+    node.next = Nnode;
+
+  }
+  public void traverseHeadList(){
+    Node node = new Node();
+    System.out.printf("List:[ ");
+    for (int i =0;i<V;i++){
+      System.out.printf("%s ",headNodeList.get(i).getTag());
+    }
+    System.out.printf("]\n");
+    //iterator
+    Iterator<Node> iterator = headNodeList.iterator();
+    while(iterator.hasNext()){
+      node = iterator.next();//iterate over headNodeList
+      //iterate over headNodeList[i]
+      System.out.printf("%s :",node.tag);
+      node = node.next;//first element of the vertex
+      while(node!=null){
+        System.out.printf("%s ",node.getWeight());
+        node = node.next;
+      }
+      System.out.println();
+    }
+
+  }
+
+  public static void main(String...args) {
+    // Graph g = new Graph(3);
+    // g.addEdge(2, 1, 2.21);
+    // g.addEdge(0, 2, 3.21);
+    // g.traverseHeadList();
+    In in = new In("src/main/data/10EWG.txt");
+    Graph g = new Graph(in);
+    g.traverseHeadList();
+  }
+}
---
+++ src/main/java/MyjavaTest/In.java
@@ -0,0 +1,67 @@
+package MyjavaTest;
+
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public final class In{
+  private Scanner scanner;//declare
+  // assume Unicode UTF-8 encoding
+  public static final String CHARSET_NAME = "UTF-8";
+  //assume language = English, country = US
+  public static final Locale LOCALE = Locale.US;
+  //Initializes an input stream from standard input
+  public In(){
+    scanner =new Scanner(new BufferedInputStream(System.in),CHARSET_NAME);
+    scanner.useLocale(LOCALE);
+  }
+  public In(String name){
+    if(name == null)throw new IllegalArgumentException("argument is null");
+    try{
+      //local file System
+      File file = new File(name);
+      if(file.exists()){
+        //warp with BufferedInputStream 
+        FileInputStream fis = new FileInputStream(file);
+        scanner = new Scanner(new BufferedInputStream(fis),CHARSET_NAME);
+        scanner.useLocale(LOCALE);
+        return;
+      }
+      else throw new IllegalArgumentException("File does not exist");
+    }
+    catch(IOException ioe){
+      throw new IllegalArgumentException("Could not open "+name,ioe);
+    }
+  }
+  public int readInt(){
+    try{
+      return scanner.nextInt();
+    }
+    catch(InputMismatchException e){
+      String token = scanner.next();
+      throw new InputMismatchException("attempt to read an 'int' value from the input stream,"+"but the next token is \""+token+"\"");
+    }
+    catch(NoSuchElementException e){
+      throw new NoSuchElementException("attempts to read an 'int' value from the input stream,"+"but no more tokens are available");
+    }
+  }
+  
+  public double readDouble() {
+    try {
+      return scanner.nextDouble();
+    } catch (InputMismatchException e) {
+      String token = scanner.next();
+      throw new InputMismatchException(
+          "attempt to read an 'int' value from the input stream," + "but the next token is \"" + token + "\"");
+    } catch (NoSuchElementException e) {
+      throw new NoSuchElementException(
+          "attempts to read an 'int' value from the input stream," + "but no more tokens are available");
+    }
+  }
+
+}
