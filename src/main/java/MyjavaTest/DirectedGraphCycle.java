2f64cdb769404c8450fd0b2b378d146b84dc84f2
164140757 <164140757@qq.com>
2019-08-19 15:06:50 +0800

    directedGraphCycle

--- src/main/java/MyjavaTest/DirectedGraphCycle.java
+++ src/main/java/MyjavaTest/DirectedGraphCycle.java
@@ -9,8 +9,25 @@ import java.util.Stack;
  */
 public class DirectedGraphCycle {
     private boolean[] marked; // marked[v] = has vertex v been marked?
-    private Node[] lastNode; // lastNode[v] = previous node on path to v
+    private String[] lastNode; // lastNode[v] = previous node on path to v
     private boolean[] onStack; // onStack[v] = is vertex on the stack?
     private Stack<Node> cycle; // directed cycle (or null if no such cycle)
-    
+
+    /**
+     * Determines whether the edge-weighted digraph{@code G} has a directed cycle and
+     * if so, find such a cycle
+     */
+    public DirectedGraphCycle(WeightedGraph G) {
+        marked = new boolean[G.V()];
+        lastNode = new String[G.V()];
+        onStack = new boolean[G.V()];
+        for (int v = 0; v < G.V(); v++) {
+            if (!marked[v]) dfs(G, v);
+        }
+    }
+    private void dfs(WeightedGraph G, int v){
+        onStack[v] = true;
+        marked[v] = true;
+        
+    }
 }
